3cffe813e6c4e5592b98c49246d185dd849945d6
jirahivy <qjimanrique@tip.edu.ph>
2021-04-15 06:05:45 +0800

    edit.

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -83,9 +83,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  function (buildAndShowHomeHTML){
-    document.querySelector("#main-content")
-      .innerHTML = responseText;
+  function (request){
+    buildAndShowHomeHTML(request);
   }, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
